146e7e972413fdcc07bb7f2a1f1886d3e0b7b243
Phan Long Thiên <90563609+PhanLongThien1914728@users.noreply.github.com>
2021-11-30 19:20:24 +0700

    Lab08 N-Tier model

---
+++ Lab08_N-Tier model/RestaurantManagementProject/DataAccess/Food.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DataAccess
+{
+    public class Food
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Unit { get; set; }
+
+        public int FoodCategoryID { get; set; }
+
+        public int Price { get; set; }
+
+        public string Notes { get; set; }
+    }
+
+    public class FoodDA
+    {
+        public List<Food> GetAll()
+        {
+            SqlConnection sqlConn = new SqlConnection(Ultilities.ConnectionString);
+            sqlConn.Open();
+
+            SqlCommand command = sqlConn.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = Ultilities.Food_GetAll;
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<Food> list = new List<Food>();
+            while (reader.Read())
+            {
+                Food food = new Food();
+                food.ID = Convert.ToInt32(reader["ID"]);
+                food.Name = reader["Name"].ToString();
+                food.Unit = reader["Unit"].ToString();
+                food.FoodCategoryID = Convert.ToInt32(reader["FoodCategoryID"]);
+                food.Price = Convert.ToInt32(reader["Price"]);
+                food.Notes = reader["Notes"].ToString();
+                list.Add(food);
+            }
+
+            sqlConn.Close();
+            return list;
+        }
+
+        public int Insert_Update_Delete(Food food, int action)
+        {
+            SqlConnection sqlConn = new SqlConnection(Ultilities.ConnectionString);
+            sqlConn.Open();
+
+            SqlCommand command = sqlConn.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = Ultilities.Food_InsertUpdateDelete;
+
+            SqlParameter IDPara = new SqlParameter("@ID", SqlDbType.Int);
+            IDPara.Direction = ParameterDirection.InputOutput;
+            command.Parameters.Add(IDPara).Value = food.ID;
+
+            command.Parameters.Add("@Name", SqlDbType.NVarChar, 1000)
+            .Value = food.Name;
+            command.Parameters.Add("@Unit", SqlDbType.NVarChar)
+            .Value = food.Unit;
+            command.Parameters.Add("@FoodCategoryID", SqlDbType.Int)
+            .Value = food.FoodCategoryID;
+            command.Parameters.Add("@Price", SqlDbType.Int)
+            .Value = food.Price;
+            command.Parameters.Add("@Notes", SqlDbType.NVarChar, 3000)
+            .Value = food.Notes;
+            command.Parameters.Add("@Action", SqlDbType.Int)
+            .Value = action;
+            int result = command.ExecuteNonQuery();
+
+            if (result > 0)
+                return (int)command.Parameters["@ID"].Value;
+            return 0;
+        }
+    }
+
+    public class FoodBL
+    {
+        FoodDA foodDA = new FoodDA();
+
+        public List<Food> GetAll()
+        {
+            return foodDA.GetAll();
+        }
+
+        public Food GetByID(int ID)
+        {
+            List<Food> list = GetAll();
+
+            foreach (var item in list)
+            {
+                if (item.ID == ID)
+                    return item;
+            }
+            return null;
+        }
+
+        public List<Food> Find(string key)
+        {
+            List<Food> list = GetAll();
+            List<Food> result = new List<Food>();
+
+            foreach (var item in list)
+            {
+                if (item.ID.ToString().Contains(key)
+                || item.Name.Contains(key)
+                || item.Unit.Contains(key)
+                || item.Price.ToString().Contains(key)
+                || item.Notes.Contains(key))
+                    result.Add(item);
+            }
+            return result;
+        }
+
+        public int Insert(Food food)
+        {
+            return foodDA.Insert_Update_Delete(food, 0);
+        }
+
+        public int Update(Food food)
+        {
+            return foodDA.Insert_Update_Delete(food, 1);
+        }
+
+        public int Delete(Food food)
+        {
+            return foodDA.Insert_Update_Delete(food, 2);
+        }
+    }
+}
---
+++ Lab08_N-Tier model/RestaurantManagementProject/RestaurantManagementProject/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace RestaurantManagementProject
+{
+    partial class frmFood
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbLeft = new System.Windows.Forms.GroupBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.cmdAdd = new System.Windows.Forms.Button();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.cbbCategory = new System.Windows.Forms.ComboBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.grpRight = new System.Windows.Forms.GroupBox();
+            this.lvsFood = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.gbLeft.SuspendLayout();
+            this.grpRight.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbLeft
+            // 
+            this.gbLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbLeft.Controls.Add(this.txtNotes);
+            this.gbLeft.Controls.Add(this.cmdDelete);
+            this.gbLeft.Controls.Add(this.cmdUpdate);
+            this.gbLeft.Controls.Add(this.cmdAdd);
+            this.gbLeft.Controls.Add(this.cmdClear);
+            this.gbLeft.Controls.Add(this.cbbCategory);
+            this.gbLeft.Controls.Add(this.txtPrice);
+            this.gbLeft.Controls.Add(this.txtUnit);
+            this.gbLeft.Controls.Add(this.txtName);
+            this.gbLeft.Controls.Add(this.label6);
+            this.gbLeft.Controls.Add(this.label4);
+            this.gbLeft.Controls.Add(this.label3);
+            this.gbLeft.Controls.Add(this.label2);
+            this.gbLeft.Controls.Add(this.label1);
+            this.gbLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbLeft.Location = new System.Drawing.Point(4, 4);
+            this.gbLeft.Name = "gbLeft";
+            this.gbLeft.Size = new System.Drawing.Size(470, 434);
+            this.gbLeft.TabIndex = 0;
+            this.gbLeft.TabStop = false;
+            this.gbLeft.Text = "Chức năng";
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(128, 165);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(257, 205);
+            this.txtNotes.TabIndex = 5;
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.Location = new System.Drawing.Point(296, 391);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(75, 23);
+            this.cmdDelete.TabIndex = 4;
+            this.cmdDelete.Text = "Xoá";
+            this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Location = new System.Drawing.Point(196, 391);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(75, 23);
+            this.cmdUpdate.TabIndex = 4;
+            this.cmdUpdate.Text = "Sửa";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // cmdAdd
+            // 
+            this.cmdAdd.Location = new System.Drawing.Point(103, 391);
+            this.cmdAdd.Name = "cmdAdd";
+            this.cmdAdd.Size = new System.Drawing.Size(75, 23);
+            this.cmdAdd.TabIndex = 4;
+            this.cmdAdd.Text = "Thêm";
+            this.cmdAdd.UseVisualStyleBackColor = true;
+            this.cmdAdd.Click += new System.EventHandler(this.cmdAdd_Click);
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(7, 391);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(75, 23);
+            this.cmdClear.TabIndex = 4;
+            this.cmdClear.Text = "Nhập lại";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // cbbCategory
+            // 
+            this.cbbCategory.FormattingEnabled = true;
+            this.cbbCategory.Location = new System.Drawing.Point(165, 126);
+            this.cbbCategory.Name = "cbbCategory";
+            this.cbbCategory.Size = new System.Drawing.Size(220, 24);
+            this.cbbCategory.TabIndex = 2;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(213, 91);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(172, 23);
+            this.txtPrice.TabIndex = 1;
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Location = new System.Drawing.Point(213, 61);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(172, 23);
+            this.txtUnit.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(128, 31);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(257, 23);
+            this.txtName.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Ghi chú";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Loại thực phẩm";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Đơn giá";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Đơn vị tính";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên thực phẩm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(10, 397);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Thống kê";
+            // 
+            // grpRight
+            // 
+            this.grpRight.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpRight.Controls.Add(this.lvsFood);
+            this.grpRight.Controls.Add(this.cmdExit);
+            this.grpRight.Controls.Add(this.label5);
+            this.grpRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpRight.Location = new System.Drawing.Point(409, 4);
+            this.grpRight.Name = "grpRight";
+            this.grpRight.Size = new System.Drawing.Size(588, 434);
+            this.grpRight.TabIndex = 1;
+            this.grpRight.TabStop = false;
+            this.grpRight.Text = "Danh mục thức ăn";
+            // 
+            // lvsFood
+            // 
+            this.lvsFood.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvsFood.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvsFood.FullRowSelect = true;
+            this.lvsFood.GridLines = true;
+            this.lvsFood.HideSelection = false;
+            this.lvsFood.Location = new System.Drawing.Point(13, 23);
+            this.lvsFood.MultiSelect = false;
+            this.lvsFood.Name = "lvsFood";
+            this.lvsFood.Size = new System.Drawing.Size(569, 368);
+            this.lvsFood.TabIndex = 0;
+            this.lvsFood.UseCompatibleStateImageBehavior = false;
+            this.lvsFood.View = System.Windows.Forms.View.Details;
+            this.lvsFood.SelectedIndexChanged += new System.EventHandler(this.lvsFood_SelectedIndexChanged);
+            this.lvsFood.Click += new System.EventHandler(this.lvsFood_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "STT";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên thực phẩm";
+            this.columnHeader2.Width = 110;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ĐVT";
+            this.columnHeader3.Width = 69;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Giá";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Loại thực phẩm";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Ghi chú";
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(513, 397);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(75, 23);
+            this.cmdExit.TabIndex = 4;
+            this.cmdExit.Text = "Thoát";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // frmFood
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 450);
+            this.Controls.Add(this.grpRight);
+            this.Controls.Add(this.gbLeft);
+            this.Name = "frmFood";
+            this.Text = "THÊM-XOÁ-SỬA BẢNG FOOD";
+            this.Load += new System.EventHandler(this.frmFood_Load);
+            this.gbLeft.ResumeLayout(false);
+            this.gbLeft.PerformLayout();
+            this.grpRight.ResumeLayout(false);
+            this.grpRight.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbLeft;
+        private System.Windows.Forms.Button cmdDelete;
+        private System.Windows.Forms.Button cmdUpdate;
+        private System.Windows.Forms.Button cmdAdd;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.ComboBox cbbCategory;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox grpRight;
+        private System.Windows.Forms.ListView lvsFood;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
